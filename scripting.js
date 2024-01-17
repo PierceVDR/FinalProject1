9448754713b09856e7f3d29e1363fc670c597c5f
PierceVDR <piercevdr@gmail.com>
2024-01-17 14:56:33 -0500

    Median Calculation

--- scripting.js
+++ scripting.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", function (p) {
   
     var entries = formData.entries();
 
-    console.log(entries);
+    // console.log(entries);
 
     // var numDice=entries[0][1];
     // var numRolls=entries[1][1];
@@ -34,8 +34,9 @@ form.addEventListener("submit", function (p) {
 function rollDice(numDice, numRolls) {
 
     occurances=new Array(5*numDice + 1).fill(0); // Array filled with an element for each outcome, begins with zeros
-    // IMPORTANT: idx 0 is the LOWEST POSSIBLE ROLL
-    totalSum=0;
+    // IMPORTANT: idx 0 is the LOWEST POSSIBLE ROLL\
+    
+    totalSum=0.0;
     rollSums=[];
     
     for (r=0; r<numRolls; r++) {
@@ -52,13 +53,26 @@ function rollDice(numDice, numRolls) {
     // MEAN:
     var mean = totalSum/numRolls;
 
-    // MEDIAN - IMPLEMENTED COMPLETELY INCORRECTLY, MUST FIX!!!
-    var median;
-    const middle=(numRolls-1)/2;
-    if (numRolls%2==1) {
-        median = rollSums[middle];
-    } else {
-        median = (rollSums[middle-0.5]+rollSums[middle+0.5]) / 2;
+    // MEDIAN:
+    var median=-1;
+    var halfwayCount = numRolls/2.0
+    var curOccurance = 0
+    while (median==-1) {
+        var numOccurances = occurances[curOccurance]
+        if (numOccurances<halfwayCount) {
+            halfwayCount-=numOccurances
+        } else if (numOccurances>halfwayCount) {
+            median=curOccurance+parseInt(numDice);
+        } else { // Means that numOccurances==halfwayCount (median is inbetween 2 outcomes)
+
+            // We have the lower number, but we need to find the next number to average them together in this scenario
+            const equalsZero = v=>v!=0;
+            const next= occurances.slice(curOccurance+1).findIndex( equalsZero ) + curOccurance+1;
+
+            median = (curOccurance+next)/2 + parseInt(numDice);
+        }
+
+        curOccurance++;
     }
 
     // FREQUENCIES:
@@ -67,6 +81,8 @@ function rollDice(numDice, numRolls) {
         frequencies[  parseInt(idx)+parseInt(numDice) ] = occurances[idx]/numRolls;
     }
 
+    console.log("RESULTS: "+rollSums);
+    console.log("OCCURANCES: "+occurances);
     console.log("MEAN: "+mean);
     console.log("MEDIAN: "+median);
     console.log("FREQS: ");
